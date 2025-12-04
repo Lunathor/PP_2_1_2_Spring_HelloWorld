93a95f04fe7c5c8108bfcb7cbe526b48830d64d9
Nikita <mrcoopgmz@gmail.com>
2025-12-04 21:12:33 +0300

    Первое задание по изучению Spring

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        
+        HelloWorld HelloWorldbean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld HelloWorldbean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat CatBean1 = (Cat) applicationContext.getBean("cat");
+        Cat CatBean2 = (Cat) applicationContext.getBean("cat");
+
+        // Сравнение бинов HelloWorld
+        System.out.println("Сравнение бинов HelloWorld: " + (HelloWorldbean1 == HelloWorldbean2));
+        // Сравнение бинов Cat
+        System.out.println("Сравнение бинов Cat: " + (CatBean1 == CatBean2));
     }
 }
